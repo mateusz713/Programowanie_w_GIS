7c45a91c51ae581290e5b239212d175db2932527
mateusz713 <mateusz713@gmail.com>
2014-10-23 10:33:49 +0200

    23_10_2014

--- cw2_zad2_Mateusz_Migon.py
+++ cw2_zad2_Mateusz_Migon.py
@@ -4,21 +4,31 @@ Created on 22-10-2014
 @author: Latino
 '''
 import string
+import types
 
 zdanieWynikowe=""
 
 def Palindrom(zdanie=""):
+    print "Wyrazenie: " +zdanie
     global zdanieWynikowe
-    zdanie.replace(" ", "")
     zdanieWynikowe = zdanie
+    zdanieWynikowe=zdanieWynikowe.replace(" ", "")
+#    i=len(zdanieWynikowe)/2
+#    while i:
+#        i-=1
+#        if zdanieWynikowe[i] != zdanieWynikowe[-i-1]:
+#            print False
+#        else:
+#            print True
+    if zdanieWynikowe[::-1] == zdanieWynikowe:
+        print True
+        print "Jest palindromem"
+    else:
+        print False 
+        print "Nie jest palindromem"
+        
     return zdanieWynikowe
 
 
-Palindrom("   aaa   bbb ccc ddd")
-print zdanieWynikowe
+Palindrom("aaa bbbb aaa")
 
-
-aaa="AAA bbb ccc"
-print aaa
-aaa.replace(" ", "")
-print aaa
